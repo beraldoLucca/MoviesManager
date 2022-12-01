f70a9e48f08ac649c365c16709cba47302d954c5
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2022-11-30 22:12:16 -0300

    first commit

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/moviesmanager/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.scl.ads.pdm.moviesmanager.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifsp.scl.ads.pdm.moviesmanager.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
